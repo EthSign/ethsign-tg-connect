c9f58ca1564c25a6c9c16caf135a16fc8100c4b7
hstarorg <hm910705@163.com>
2023-08-15 22:21:19 +0800

    chore: init projrct

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tg-brother
 Proxy tg login site for restricted domain
+
+
+## Project Setup
+
+```sh
+pnpm i
+
+# Compile and Hot-Reload for Development
+pnpm dev
+
+# Type-Check, Compile and Minify for Production
+pnpm build
+
+# Run Unit Tests with [Vitest](https://vitest.dev/)
+pnpm test:unit
+
+# Lint with [ESLint](https://eslint.org/)
+pnpm lint
+```
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+  <main ref="mainDomRef">
+
+  </main>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { parseQuery } from 'vue-router'
+
+const mainDomRef = ref<HTMLElement | null>(null);
+
+(window as any).onTelegramAuth = function (user) {
+  console.log('login ', user)
+}
+
+onMounted(() => {
+  const query = parseQuery(window.location.search);
+  const botName = query.bot_name as string || '';
+
+  if (!botName) {
+    document.write('bot_name is required');
+  }
+
+  const tgScriptEl = document.createElement('script');
+  tgScriptEl.setAttributeNS(null, 'async', '');
+  tgScriptEl.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?22');
+  tgScriptEl.setAttribute('data-telegram-login', botName);
+  tgScriptEl.setAttribute('data-size', 'large');
+  tgScriptEl.setAttribute('data-radius', '20');
+  tgScriptEl.setAttribute('data-onauth', 'onTelegramAuth(user)');
+  tgScriptEl.setAttribute('data-request-access', 'write');
+
+  mainDomRef.value?.appendChild(tgScriptEl);
+})
+
+</script>
+
+<style scoped></style>
